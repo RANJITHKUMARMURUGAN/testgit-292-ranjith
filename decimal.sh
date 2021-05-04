ef465110200eb08fe6e9fdfae8832cad7b9c519d
RANJITHKUMARMURUGAN <ranjithm1193@gmail.com>
2021-05-04 13:05:59 +0530

    [Ranjith] added decimal.sh

--- decimal.sh
+++ decimal.sh
@@ -4,6 +4,8 @@ b=6;
 division=`echo $a $b | awk '{print $1/$2}'`
 multiplication=$(( a*b ))
 power=$(( a**b ))
+subtraction=$(( a-b));
+echo $subtraction;
 echo $power;
 echo $division
 echo $multiplication
